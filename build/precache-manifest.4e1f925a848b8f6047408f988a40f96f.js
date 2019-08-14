84407ffb1b616c22afbd20bd3e559209c680c58b
unknown <sunilj74@gmail.com>
2019-08-14 16:29:55 -0400

    build added

---
+++ build/precache-manifest.4e1f925a848b8f6047408f988a40f96f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2066dda57a3040c5088513b87bb46498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ecb37eb9506d83d6ca",
+    "url": "/static/css/main.2f02ffbb.chunk.css"
+  },
+  {
+    "revision": "bf250da80d88ced14a18",
+    "url": "/static/js/2.681faf79.chunk.js"
+  },
+  {
+    "revision": "34ecb37eb9506d83d6ca",
+    "url": "/static/js/main.476e9d31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26f4b6d8cacc7b902a6729e38b5e4d5d",
+    "url": "/static/media/bg.26f4b6d8.jpg"
+  },
+  {
+    "revision": "a08d3e7bff4c5c1ab815860cd0fffa25",
+    "url": "/static/media/cover2.a08d3e7b.jpg"
+  },
+  {
+    "revision": "228a308f0165ce1de750ba85e8a09f76",
+    "url": "/static/media/shopstuff.228a308f.gif"
+  }
+]);
---
+++ initme.bat
@@ -0,0 +1,6 @@
+echo "# cattitude" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sunilj74/cattitude.git
+git push -u origin master
