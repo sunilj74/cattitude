7ba6285bd82acf6186635302d7557773dc7130e9
unknown <sunilj74@gmail.com>
2019-08-14 20:58:25 -0400

    build added

---
+++ precache-manifest.df3c9eaaa995472df364fe0bfa18ed80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84bfc96a732ff84769766a353ab13d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a75bc3a9e884ad4a14",
+    "url": "/static/css/main.78013e97.chunk.css"
+  },
+  {
+    "revision": "84d597978cb405ff0ac7",
+    "url": "/static/js/2.2e680b48.chunk.js"
+  },
+  {
+    "revision": "76a75bc3a9e884ad4a14",
+    "url": "/static/js/main.1f1e90cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26f4b6d8cacc7b902a6729e38b5e4d5d",
+    "url": "/static/media/bg.26f4b6d8.jpg"
+  },
+  {
+    "revision": "a08d3e7bff4c5c1ab815860cd0fffa25",
+    "url": "/static/media/cover2.a08d3e7b.jpg"
+  }
+]);
