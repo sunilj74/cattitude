2ebe870175ee29cb3edd3748e57f6cbf1415c84e
unknown <sunilj74@gmail.com>
2019-08-20 14:35:55 -0400

    responsive feature added

---
+++ precache-manifest.83ba956a614688850551fb2bd5b4f232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cd1ddbd2e5da330aad2bef21529470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f48e4ff2626e2957030",
+    "url": "/static/css/main.12b77f7c.chunk.css"
+  },
+  {
+    "revision": "dc8989b53a484a0c2e68",
+    "url": "/static/js/2.dfdec928.chunk.js"
+  },
+  {
+    "revision": "9f48e4ff2626e2957030",
+    "url": "/static/js/main.c01116f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26f4b6d8cacc7b902a6729e38b5e4d5d",
+    "url": "/static/media/bg.26f4b6d8.jpg"
+  },
+  {
+    "revision": "a08d3e7bff4c5c1ab815860cd0fffa25",
+    "url": "/static/media/cover2.a08d3e7b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df3c9eaaa995472df364fe0bfa18ed80.js"
+  "/precache-manifest.83ba956a614688850551fb2bd5b4f232.js"
 );
 
 self.addEventListener('message', (event) => {
